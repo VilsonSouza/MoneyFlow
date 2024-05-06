7791b216923cf6a63330d869062e6b56abfc4bed
vilson souza <vilsonsouza758@gmail.com>
2024-05-06 10:17:42 -0300

    first commit

---
+++ src/model/dao/UsuarioDAO.java
@@ -0,0 +1,247 @@
+package model.dao;
+
+import java.security.Key;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Base64;
+
+import model.dao.ConexaoBD;
+import model.vo.MovimentacaoVO;
+import model.vo.RelatorioAnualVO;
+import model.vo.UsuarioVO;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class UsuarioDAO {
+	
+	private static final String CHAVE = "0qoVmtNcUUvhLhPZ";
+    
+    public boolean verificaUsuario(UsuarioVO usuarioVO) {
+    	
+    	String senhaEncriptografada = criptografar(usuarioVO.getSenha());
+    	
+    	int qtd = 0;
+
+        String comandoSQL = "SELECT count(*) FROM usuario WHERE email = '"
+        		+ usuarioVO.getEmail()
+        		+ "' AND senha = '"
+        		+ senhaEncriptografada
+        		+ "';";
+
+        try (Statement comando = ConexaoBD.getConexaoBD().createStatement();
+             ResultSet resultado = comando.executeQuery(comandoSQL)) {
+
+            while (resultado.next()) {
+                qtd = resultado.getInt(1);
+            }
+            
+            resultado.close();
+            comando.close();
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao realizar conexão com o banco de dados.");
+            e.printStackTrace();
+        }
+
+        if(qtd != 0)
+        	return true;
+        else
+        	return false;
+    }
+    
+    public boolean verificaUsuario(String email) {
+    	
+    	int qtd = 0;
+
+        String comandoSQL = "SELECT count(*) FROM usuario WHERE email = '"
+        		+ email
+        		+ "';";
+
+        try (Statement comando = ConexaoBD.getConexaoBD().createStatement();
+             ResultSet resultado = comando.executeQuery(comandoSQL)) {
+
+            while (resultado.next()) {
+                qtd = resultado.getInt(1);
+            }
+            
+            resultado.close();
+            comando.close();
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao realizar conexão com o banco de dados.");
+            e.printStackTrace();
+        }
+
+        if(qtd != 0)
+        	return true;
+        else
+        	return false;
+    }
+
+    public boolean addUsuario(UsuarioVO usuarioVO) {
+    	
+    	int linhasAfetadas = 0;
+
+        String comandoSQL = "INSERT INTO usuario (email, senha, nome) VALUES (?, ?, ?);";
+
+        try (PreparedStatement comando = ConexaoBD.getConexaoBD().prepareStatement(comandoSQL)) {
+            comando.setString(1, usuarioVO.getEmail());
+            comando.setString(2, criptografar(usuarioVO.getSenha()));
+            comando.setString(3, usuarioVO.getNome());
+
+            linhasAfetadas = comando.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao adicionar usuário no banco de dados.");
+            e.printStackTrace();
+        }
+        
+        if(linhasAfetadas > 0)
+        	return true;
+    	else
+    		return false;        		
+    }
+    public boolean alterUsuario(UsuarioVO usuarioVO) {
+        int linhasAfetadas = 0;
+
+        String comandoSQL = "UPDATE usuario SET senha = ?, nome = ? WHERE email = ?;";
+
+        try (PreparedStatement comando = ConexaoBD.getConexaoBD().prepareStatement(comandoSQL)) {
+            comando.setString(1, criptografar(usuarioVO.getSenha())); // Criptografa a nova senha
+            comando.setString(2, usuarioVO.getNome());
+            comando.setString(3, usuarioVO.getEmail());
+
+            linhasAfetadas = comando.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao alterar usuário no banco de dados.");
+            e.printStackTrace();
+        }
+
+        return linhasAfetadas > 0;
+    }
+    public boolean delUsuario(UsuarioVO usuarioVO) {
+        int linhasAfetadas = 0;
+
+        // Exclua primeiro os registros relacionados nas tabelas filhas
+        try (Connection conexao = ConexaoBD.getConexaoBD()) {
+            // Desative o autocommit para garantir que as exclusões possam ser revertidas se algo der errado
+            conexao.setAutoCommit(false);
+
+            // Exclua os registros relacionados à movimentacao
+            String comandoMovimentacao = "DELETE FROM movimentacao WHERE emailUsuario = ?";
+            try (PreparedStatement stmtMovimentacao = conexao.prepareStatement(comandoMovimentacao)) {
+                stmtMovimentacao.setString(1, usuarioVO.getEmail());
+                stmtMovimentacao.executeUpdate();
+            }
+
+            // Exclua os registros relacionados ao banco
+            String comandoBanco = "DELETE FROM banco WHERE emailUsuario = ?";
+            try (PreparedStatement stmtBanco = conexao.prepareStatement(comandoBanco)) {
+                stmtBanco.setString(1, usuarioVO.getEmail());
+                stmtBanco.executeUpdate();
+            }
+
+            // Exclua os registros relacionados à categoria
+            String comandoCategoria = "DELETE FROM categoria WHERE emailUsuario = ?";
+            try (PreparedStatement stmtCategoria = conexao.prepareStatement(comandoCategoria)) {
+                stmtCategoria.setString(1, usuarioVO.getEmail());
+                stmtCategoria.executeUpdate();
+            }
+
+            // Exclua os registros relacionados à meta
+            String comandoMeta = "DELETE FROM meta WHERE emailUsuario = ?";
+            try (PreparedStatement stmtMeta = conexao.prepareStatement(comandoMeta)) {
+                stmtMeta.setString(1, usuarioVO.getEmail());
+                stmtMeta.executeUpdate();
+            }
+
+            // Exclua os registros relacionados ao quadro
+            String comandoQuadro = "DELETE FROM quadro WHERE emailUsuario = ?";
+            try (PreparedStatement stmtQuadro = conexao.prepareStatement(comandoQuadro)) {
+                stmtQuadro.setString(1, usuarioVO.getEmail());
+                stmtQuadro.executeUpdate();
+            }
+
+            // Exclua os registros relacionados ao alerta
+            String comandoAlerta = "DELETE FROM alerta WHERE emailUsuario = ?";
+            try (PreparedStatement stmtAlerta = conexao.prepareStatement(comandoAlerta)) {
+                stmtAlerta.setString(1, usuarioVO.getEmail());
+                stmtAlerta.executeUpdate();
+            }
+
+            // Exclua o registro do usuário principal
+            String comandoUsuario = "DELETE FROM usuario WHERE email = ?";
+            try (PreparedStatement stmtUsuario = conexao.prepareStatement(comandoUsuario)) {
+                stmtUsuario.setString(1, usuarioVO.getEmail());
+                linhasAfetadas = stmtUsuario.executeUpdate();
+            }
+
+            // Se chegou até aqui sem lançar exceções, confirme as alterações
+            conexao.commit();
+        } catch (SQLException e) {
+            // Se algo der errado, faça rollback das alterações e registre o erro
+            System.err.println("Erro ao excluir usuário do banco de dados.");
+            e.printStackTrace();
+
+        }
+
+        return linhasAfetadas > 0;
+    }
+
+
+
+
+
+    private String criptografar(String senha) {
+        try {
+
+            SecretKey chaveSecreta = new SecretKeySpec(CHAVE.getBytes(), "AES");
+            Cipher cifra = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cifra.init(Cipher.ENCRYPT_MODE, chaveSecreta);
+
+
+            byte[] textoCriptografado = cifra.doFinal(new String(senha).getBytes());
+
+            return Base64.getEncoder().encodeToString(textoCriptografado);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+	public ArrayList<RelatorioAnualVO> getRelatorioAnual(String emailUsuario) {
+		RelatorioAnualVO relatorioVO;
+		ArrayList<RelatorioAnualVO> dados = new ArrayList<RelatorioAnualVO>();
+
+		String comandoSQL = "SELECT MONTH(m.data_ocorrencia) AS mes, SUM(m.valor_total) AS valorTotal FROM movimentacao m JOIN usuario u ON m.emailUsuario = u.email WHERE YEAR(m.data_ocorrencia) = YEAR(CURRENT_DATE()) AND u.email = '"
+				+ emailUsuario
+				+ "' GROUP BY MONTH(m.data_ocorrencia) ORDER BY mes;";
+
+		try {
+			Statement comando = ConexaoBD.getConexaoBD().createStatement();
+			ResultSet resultado = comando.executeQuery(comandoSQL);
+
+			while(resultado.next()) {				
+				relatorioVO = new RelatorioAnualVO();
+				relatorioVO.setNumeroMes(resultado.getInt("mes"));
+				relatorioVO.setValorTotalMes(resultado.getFloat("valorTotal"));
+				
+				dados.add(relatorioVO);
+			}
+
+			resultado.close();
+			comando.close();
+
+		}catch (SQLException e) {
+			System.err.println("Erro ao realizar conexao com o banco "
+					+ "verifique a url de conexão");
+			e.printStackTrace();
+		}
+
+		return dados;
+	}
+}
---
+++ src/view/GerenciamentoPerfilVisao.java
@@ -0,0 +1,208 @@
+package view;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import controller.MoneyFlowController;
+import model.vo.UsuarioVO;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GerenciamentoPerfilVisao extends JInternalFrame {
+    private MoneyFlowController controller;
+    private UsuarioVO usuarioVO;
+
+    private JButton buttonEditar;
+    private JButton buttonCancelar;
+    private JButton buttonExcluir;
+
+
+    private JTextField textNome;
+
+    private JPasswordField textSenha;
+    private JDesktopPane desktop;
+
+    private JLabel labelLogin;
+
+
+
+    private ImageIcon logo;
+
+
+    public GerenciamentoPerfilVisao( JDesktopPane desktop, MoneyFlowController controller, String email, UsuarioVO usuarioVO) {
+        super("Editar Perfil");
+
+        this.logo = new ImageIcon("icons/logo_app.png");
+        this.controller = controller;
+        this.usuarioVO = usuarioVO;
+        this.desktop = desktop;
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");	// Java Swing Nimbus
+        }
+        catch (Exception e)
+        { }
+
+        //chama o metodo rsponsavel por inicializar os componentes
+        inicializaComponentes();
+
+        //monta todo o layout e em seguida o adiciona
+        this.getContentPane().add(this.montaPainel());
+
+        buttonEditar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                salvar();
+            }
+        });
+
+
+        buttonCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancelar();
+            }
+        });
+        buttonExcluir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                excluir();
+            }
+        });
+
+    }
+    private void inicializaComponentes() {
+
+        buttonEditar = new JButton("Editar");
+        buttonCancelar = new JButton("Cancelar");
+        buttonExcluir = new JButton("Excluir");
+
+
+
+        textNome = new JTextField();
+        textSenha = new JPasswordField();
+
+        labelLogin = new JLabel("Edição do Perfil");
+
+
+        textNome.setText(this.usuarioVO.getNome());
+        textSenha.setText("senha"); if (usuarioVO != null) {
+            textNome = new JTextField(usuarioVO.getNome());
+            textSenha = new JPasswordField();
+        } else {
+
+            textNome = new JTextField();
+            textSenha = new JPasswordField();
+        }
+
+    }
+    private JComponent montaPainel() {
+        FormLayout layout = new FormLayout(
+                "10dlu:grow, p, center:p:grow, p, 10dlu:grow",
+                "10dlu:grow, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 5dlu, p, 10dlu:grow");
+
+        // Agrupe as colunas para que tenham o mesmo tamanho
+        layout.setColumnGroups(new int[][]{{2, 4}});
+
+        DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+
+        CellConstraints cc = new CellConstraints();
+
+        builder.add(labelLogin, cc.xy(3, 2));
+
+
+
+        builder.addSeparator("Digite os Novos Dados", cc.xyw(2, 10, 3));
+
+        builder.addLabel("Nome:", cc.xy(2, 12));
+        builder.add(textNome, cc.xyw(2, 14, 3));
+
+        builder.addLabel("Senha:", cc.xy(2, 16));
+        builder.add(textSenha, cc.xyw(2, 18, 3));
+
+
+
+        builder.add(buttonEditar, cc.xyw(2, 22, 3));
+        builder.add(buttonCancelar, cc.xyw(2, 24, 3));
+        builder.add(buttonExcluir, cc.xyw(2, 25, 3));
+
+
+
+        return builder.getPanel();
+    }
+    private void salvar() {
+        String nome = textNome.getText();
+        String senha = new String(textSenha.getPassword());
+
+        if (nome.isEmpty() && senha.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Preencha pelo menos um campo.", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (!nome.isEmpty()) {
+            usuarioVO.setNome(nome);
+        }
+
+        if (!senha.isEmpty()) {
+            usuarioVO.setSenha(senha);
+        }
+
+        if (controller.alterUsuario(usuarioVO)) {
+            JOptionPane.showMessageDialog(this, "Perfil atualizado com sucesso.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+            voltar();
+        } else {
+            JOptionPane.showMessageDialog(this, "Falha ao atualizar o perfil.", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    private void excluir() {
+        int opc = JOptionPane.showConfirmDialog(GerenciamentoPerfilVisao.this, "Tem certeza que deseja excluir o perfil?", "", JOptionPane.WARNING_MESSAGE);
+
+        if (opc == 0) {
+            // Excluir o perfil
+            if (controller.delUsuario(usuarioVO)) {
+                JOptionPane.showMessageDialog(GerenciamentoPerfilVisao.this, "Perfil excluído com sucesso.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+
+                // Fechar a janela atual (GerenciamentoPerfilVisao)
+                dispose();
+
+                // Fechar a janela do MenuVisao (se estiver aberta)
+                for (Frame frame : Frame.getFrames()) {
+                    if (frame instanceof MenuVisao) {
+                        frame.dispose();
+                        break;
+                    }
+                }
+
+                // Abrir a janela de LoginVisao
+                LoginVisao loginVisao = new LoginVisao(logo, controller, null);
+                loginVisao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                loginVisao.setBounds(100, 100, 320, 450);
+                loginVisao.setIconImage(logo.getImage());
+                loginVisao.setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(GerenciamentoPerfilVisao.this, "Erro ao excluir o perfil.", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+
+    private void cancelar() {
+        int opc = JOptionPane.showConfirmDialog(GerenciamentoPerfilVisao.this, "Tem certeza que deseja cancelar?", "", JOptionPane.WARNING_MESSAGE);
+
+        if(opc == 0)
+            voltar();
+    }
+    private void voltar() {
+        this.dispose();
+    }
+
+    public void setPosicao() {
+        Dimension d = this.desktop.getSize();
+        this.setLocation((d.width - this.getSize().width) / 2, (d.height - this.getSize().height) / 2);
+    }
+
+}
